0662810349c2bb6ddfafc8a571b141d310145d38
Srikanth Ramu <srikanthramu@Srikanths-MacBook-Air.local>
2022-07-03 16:45:43 -0700

    added code comments

--- src/main/java/burp/webauthncbor/Util.java
+++ src/main/java/burp/webauthncbor/Util.java
@@ -20,15 +20,19 @@ public class Util {
      * @return
      */
     public static byte[] getDecodedAttestObjectArray(byte[] cborArray){
+        // Check if the attestationObject value is empty
         if (cborArray != null && cborArray.length > 0) {
+            // Ensure the out is propery indented
             ObjectMapper objectMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
             ObjectConverter objectConverter = new ObjectConverter(objectMapper, new ObjectMapper(new CBORFactory()));
             CborConverter cborConverter = objectConverter.getCborConverter();
+            // Decode the CBOR using a library
             AttestationObject attestObj = cborConverter.readValue(cborArray, AttestationObject.class);        
             JsonConverter jsonConverter = objectConverter.getJsonConverter();
             byte[] decodedAttestObjArray = jsonConverter.writeValueAsString(attestObj).getBytes();
             return decodedAttestObjArray;
         }
+        // Return error message when valid attestationObject CBOR data is not found
         return Constants.ERROR_MSG.getBytes();
     }
 }
